6e58bfe6e616f39a016a120946e4e842e4b4b3d3
RaulPPealez <raul.perez@uam.es>
2025-09-26 13:35:49 +0200

    fix: switch to char*

--- src/lessons/7-struct/7-size2.c
+++ src/lessons/7-struct/7-size2.c
@@ -14,10 +14,10 @@ int main() {
   s.c = 'C';
   printf("a: %c, b: %d, c: %c\n", s.a, s.b, s.c); // a: A, b: 42, c: C
   // The address of b is 4 bytes after the address of a
-  printf("Address of a: %p\n", (void *)&s.a);
-  printf("Address of b: %p\n", (void *)&s.b);
-  printf("Address of a + 4 bytes: %p\n", (void *)&s.a + 4);
-  printf("Contents of &s+4 (%p) as int: %d\n", (void *)&s.a + 4,
+  printf("Address of a: %p\n", (char *)&s.a);
+  printf("Address of b: %p\n", (char *)&s.b);
+  printf("Address of a + 4 bytes: %p\n", (char *)&s.a + 4);
+  printf("Contents of &s+4 (%p) as int: %d\n", (char *)&s.a + 4,
          *(int *)((char *)&s.a + 4));
   printf("Contents of &s+4+4 as char: %c\n", *(char *)((char *)&s.a + 4 + 4));
   return 0;
